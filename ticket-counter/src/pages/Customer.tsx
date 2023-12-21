6cab0c11260d07ac86766f8a7806d4ee6842f68d
khawarizmi02 <117525739+khawarizmi02@users.noreply.github.com>
2023-12-21 08:17:31 +0800

    Update Customer.tsx

--- ticket-counter/src/pages/Customer.tsx
+++ ticket-counter/src/pages/Customer.tsx
@@ -1,7 +1,6 @@
 // import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import data from "../data.json";
 import { Counter } from "../components";
 import { useAuth } from "../Auth";
 
